58885c39ae60de227cd2b18dfc28c2bd690706f7
advait2711 <137433670+advait2711@users.noreply.github.com>
2024-10-26 01:20:22 +0530

    Update wsgi.py

--- DevTest/DevTest/wsgi.py
+++ DevTest/DevTest/wsgi.py
@@ -15,7 +15,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DevTest.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DevTest.DevTest.settings')
 
 application = get_wsgi_application()
 
