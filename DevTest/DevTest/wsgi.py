013559f7a59e71c71e27dda80709c00922d9b84c
advait2711 <137433670+advait2711@users.noreply.github.com>
2024-10-26 01:16:50 +0530

    Update wsgi.py

--- DevTest/DevTest/wsgi.py
+++ DevTest/DevTest/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+# Add the project directory to the PYTHONPATH
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DevTest.DevTest.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DevTest.settings')
 
 application = get_wsgi_application()
+
