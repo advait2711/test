fa34cfdf0a8df9ddd203968e9ee5c5553a813f56
advait2711 <137433670+advait2711@users.noreply.github.com>
2024-10-27 00:44:43 +0530

    Update urls.py

--- DevTest/home/urls.py
+++ DevTest/home/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),         # Home page URL
-    path('resume/', views.gen_resume, name='resume'),  # Resume generation URL
-    path('upload/', views.upload_file, name='upload_file'),  # File upload URL
+    path('upload/', views.upload_file, name='upload_file'),
+    
 ]
+
